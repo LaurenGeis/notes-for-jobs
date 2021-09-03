7a56ad917ea47810e78997be21dbbc6fc0902d97
Lauren Geislinger <72772740+LaurenGeis@users.noreply.github.com>
2021-09-03 11:28:38 -0700

    Add files via upload
    This is just the program file, not the full project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// imports at very top
+using System;
+
+
+namespace Test  // project name
+{
+    class Program   // class container 
+    {
+        static void Main(string[] args)     // main method 
+        {
+            Console.WriteLine("This is a C# test, for notes on C#");  // Console.WriteLine is the print function in C# 
+            
+
+            int index = 1;
+            while (index <= 5)  // while loop test
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.ReadLine(); // keeps console open
+        }
+    }
+}
